5b10c056da46cd177a899b58eda41f9e1946478d
Anderson-Lai <andersonmhlai26@gmail.com>
2024-09-13 23:11:26 -0400

    time printing

--- include/private/timing.h
+++ include/private/timing.h
@@ -6,4 +6,6 @@ namespace Timing
     std::chrono::time_point<std::chrono::high_resolution_clock> Now();
     void Sleep(const unsigned long long milliseconds);
     unsigned long long SecondsSinceEpoch();
+    void Log(const std::chrono::time_point<std::chrono::high_resolution_clock>& first,
+            const std::chrono::time_point<std::chrono::high_resolution_clock>& second);
 }
--- src/timing.cpp
+++ src/timing.cpp
@@ -1,5 +1,6 @@
 #include "timing.h"
 #include <thread>
+#include "log.h"
 
 std::chrono::time_point<std::chrono::high_resolution_clock> Timing::Now()
 {
@@ -15,4 +16,11 @@ unsigned long long Timing::SecondsSinceEpoch()
 {
     const std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
     return std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count();
+}
+
+void Timing::Log(const std::chrono::time_point<std::chrono::high_resolution_clock>& start, 
+        const std::chrono::time_point<std::chrono::high_resolution_clock>& end)
+{
+    const std::chrono::milliseconds elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    Log::Println(std::format("Time Elapsed: {}", elapsed));
 }
